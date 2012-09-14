6cb85db4c247f2291251ae8f13ac9b2b684d2c16
Christian Klammer <christian@sct.com.au>
2012-09-14 17:13:39 +1000

    working on default form markup

--- js/form_helper.js
+++ js/form_helper.js
@@ -8,11 +8,14 @@ $(function(){
 
 var FormHelper = (function(){
 
-	var _self = {};
+	var _self = {},
+		_forms;
 
 
 
 	function init(){
+		console.log('INIT');
+		_forms = $('form');
 
 		// add placeholder functionality to text inputs
 		// the placeholder text should be set as the value and data attribute:
@@ -30,13 +33,18 @@ var FormHelper = (function(){
 		// </form>
 
 		formActionReplacement();
+
+
+		// Form validation using jQuery Validate plugin (in plugin.js)
+		// Replace with custom function if needed
+		validateForms();
 	}
 
 
 
 	function formPlaceholder(){
 
-		$('form').on('focus','input[type=text]',function(){
+		_forms.on('focus','input[type=text]',function(){
 
 			var _input       = $(this),
 				_val         = $.trim(_input.val()),
@@ -62,7 +70,7 @@ var FormHelper = (function(){
 
 
 	function formActionReplacement(){
-		$('form').each(function(){
+		_forms.each(function(){
 			var _form = $(this),
 				_formAction = _form.find('.form_action');
 
@@ -75,6 +83,66 @@ var FormHelper = (function(){
 		});
 	}
 
+
+	function validateForms(){
+		console.log('validate');
+		_forms.validate({
+			// Change the default error element to <em> for easy hiding with CSS if not required
+			errorElement: "em",
+			
+			submitHandler: function(){
+				alert('Form validates. Do ajaxy stuff or remove the "submitHander" option from the validate plugin!');
+			},
+
+			//Adds "error" class to input label
+			highlight: function(element, errorClass, validClass) {
+				toggleHighlight(element,errorClass,validClass,false);
+			},
+		
+			//Removes "error" class to input label
+			unhighlight: function(element, errorClass, validClass) {
+				toggleHighlight(element,errorClass,validClass,true);
+			},
+
+			errorPlacement: function(error, element) {
+				var _type = element.attr('type');
+				console.log('errorPlacement');
+
+				if( _type === 'checkbox' ){
+					// do checkbox specific stuff
+					// return;
+				}
+				else if( _type === 'radio' ){
+					// do radio specific stuff
+					// return;
+				}
+
+				element.closest('.form_item').append(error);
+			}
+
+		});
+
+	}
+
+
+	function toggleHighlight(element,errorClass,validClass,isValid){
+		
+		var type          = $(element).attr('type'),
+			classToAdd    = isValid ? validClass : errorClass,
+			classToRemove = isValid ? errorClass : validClass;
+		
+		if(type === "checkbox"){
+			// do stuff if it's a checkbox 
+			// return;
+		}
+		
+		if(type === "radio"){
+			// do stuff if it's a radio button
+			// return;
+		}
+		
+		$(element).closest('.form_item').removeClass(classToRemove).addClass(classToAdd);
+	}
 
 
 	_self = {
